accf3e4b4cb94fde7d4a6946af4c34062aa394a1
zion <130053047+tsitiii@users.noreply.github.com>
2024-03-01 22:17:36 +0300

    Delete week7 directory

--- week7/GDSCBlog/CommentApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from BlogApp.models import Postt
-from django.utils import timezone
-class Comment(models.Model):
-    Content=models.TextField(max_length=250)
-    Author=models.CharField(max_length=50)
-    PublishedDate=models.DateTimeField(default=timezone.now)
-    Post=models.ForeignKey(Postt,  on_delete=models.CASCADE)
-
